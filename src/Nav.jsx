7c1c0fbddce6864000c95fcf0b16deb37d460892
nanditha <nandithahari6@gmail.com>
2023-10-01 18:09:25 +0530

    Added css

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,17 +1,17 @@
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
 
 function Nav() {
     return (
       <>
       <div className="nav">
-            <Link to="/profile">Profile</Link>
-            <Link to="/loandetails">Loan Details</Link>
-            <Link to="/grievance">Grievance Redressal </Link>
+            <Link className="link" to="/profile">Profile</Link>
+            <Link className="link" to="/loandetails">Loan Details</Link>
+            <Link className="link" to="/grievance">Grievance Redressal </Link>
       </div>
        
       </>
     )
   }
   
-  export default Nav
+  export default Nav;
   
